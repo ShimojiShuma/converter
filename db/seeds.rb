6f0fee51dbe08075f6ca33979f1f1e98fdf8aff0
NMS_mac_004 <nms_mac_004@NMS-mac-004noMacBook-Air.local>
2021-03-15 19:54:49 +0900

    2nd commit

---
+++ db/seeds.rb
@@ -0,0 +1,4 @@
+Information.create("title":"A - XX電鉄","content":"只今、30分以上の列車の遅れはございません。","line":"A線","delay":false)
+Information.create("title":"B - XX電鉄","content":"只今、30分以上の列車の遅れはございません。","line":"B線","delay":false)
+Information.create("title":"C - XX電鉄","content":"只今、30分以上の列車の遅れはございません。","line":"C線","delay":false)
+Information.create("title":"D - XX電鉄","content":"只今、30分以上の列車の遅れはございません。","line":"D線","delay":false)
